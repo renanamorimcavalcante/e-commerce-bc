19e1f88200633848ab69db531bfce3d637fdf014
Renan Cavalcante <renan.acavalcante@gmail.com>
2025-08-05 00:01:18 -0300

    chore: add eslint, prettier and shadcn setup

---
+++ src/app/product/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Product = () => {
+  return (
+    <div>
+      <h1>RENAN PRODUCT!</h1>
+      <Button>BUTTON</Button>
+    </div>
+  );
+};
+
+export default Product;
