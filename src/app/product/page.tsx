59a31d1b68489d830a945807873645d998c1995f
Renan Cavalcante <renan.acavalcante@gmail.com>
2025-08-09 10:03:48 -0300

    Commit-2

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 const Product = () => {
   return (
     <div>
-      <h1>RENAN PRODUCT!</h1>
-      <Button>BUTTON N1</Button>
+      <h1>Product</h1>
     </div>
   );
 };
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,18 @@
-// Better Auth
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-// Drizzle ORM
-import { db } from "../db/index";
-import * as schema from "../db/schema";
+import { db } from "@/db";
+import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   database: drizzleAdapter(db, {
     provider: "pg",
@@ -22,5 +26,8 @@ export const auth = betterAuth({
   },
   account: {
     modelName: "accountTable",
+  },
+  verification: {
+    modelName: "verificationTable",
   },
 });
